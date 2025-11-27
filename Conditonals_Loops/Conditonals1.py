661c2f589ad0cd812bc5048d8cb57137a4ab9f57
SupriyaPE <supriyape2002@gmail.com>
2025-11-28 01:02:48 +0530

    Working on Python Conditonals and Loops

---
+++ Conditonals_Loops/Conditonals1.py
@@ -0,0 +1,78 @@
+# weather = "hot"
+
+# if weather=="rainy" or weather=="hot":
+#     print("Use Cab")
+
+# elif weather == "Normal":
+#     print("Use Bike")
+
+# else :
+#     print("Go by walk")
+
+# *****************************************************************************
+
+
+# file = "java.zip"
+
+# if file.endswith("exe"):
+#     print("execute it")
+
+# if file.endswith("jpg"):
+#     print("Upload it!!")
+
+# if file.endswith("pdf"):
+#     print("copy it some folder!!")
+
+# if file.endswith("zip") or file.endswith("jar"):
+#     print("extract it!!")
+
+# else:
+#     print("Invalid file format!!")
+
+
+# *****************************************************************************
+
+
+# marks = 101
+
+# if marks < 35:
+#     print("failed")
+# elif marks < 50:
+#     print("C grade")
+# elif marks < 60:
+#     print("B grade")
+# elif marks < 80:
+#     print("A grade")
+# elif marks <= 100:
+#     print("Topper")
+# else:
+#      print("invalid number")
+    
+
+# *****************************************************************************
+
+
+# x = 3000
+# if x>=100:
+#     if x > 1000:
+#         print("its greater than thousand")
+#     else:
+#         print("its in hundred")
+# else:
+#     print("its less than thousand")
+
+
+# *****************************************************************************
+
+
+# x = 65
+# if isinstance(x,list):
+#     if x >= 100:
+#        if x > 1000:
+#           print("its greater than thousand")
+#        else: 
+#         print("its in hundrend")
+#     else:
+#         print("its less than hundreds")
+# else:
+#    print("enter a valid number")
---
+++ Conditonals_Loops/Loops1.py
@@ -0,0 +1,67 @@
+# for i in range(10):
+#     print(i)
+#     if i == 5:
+#         break
+ 
+
+
+# for i in range(10):
+#     if i == 5:
+#         break
+#     print(i)
+
+# for i in range(5):
+#     print(i)
+# else:
+#     print("no block occured")
+
+
+
+# for i in range(3):
+#    for j in range(2):
+#     print(i,j)
+  
+
+# for el in range(10):
+#     print(el)
+#     break
+#     if el == 3:
+#        continue
+#     print("====>>>>\n")
+  
+# print("execution completed")
+
+
+
+# for i in range(5):
+#     if i == 2:
+#         continue
+#     print(i)
+
+
+# for i in range(10):
+#     print(i)
+#     continue
+#     if el == 3:
+#         continue
+#     print("=====>>>>\n")
+# print("execution completed")
+
+
+
+# countries = ["IND","CHINA","SL","BAN"]
+
+# for i,el in enumerate(countries):
+#     print(f"element at position {i} is {el}")
+
+# for i in range(5):
+#     pass
+
+
+
+# count = 0
+# while count<=5:
+#     print (count)
+#     count+=1
+
+
